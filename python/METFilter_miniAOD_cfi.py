c78116255bb100d7dd205afb80b26b1b44a95f55
khaosmos93 <khaosmos93@gmail.com>
2018-07-18 07:25:23 +0200

    update python

--- python/METFilter_miniAOD_cfi.py
+++ python/METFilter_miniAOD_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 defaultSelector = cms.EDFilter("METFilter_miniAOD",
 #                               src = cms.InputTag("TriggerResults","","PAT"),	# to submit MC
-                                src = cms.InputTag("TriggerResults","","RECO"), # to submit data
+                               src = cms.InputTag("TriggerResults","","RECO"), # to submit data
                                flag = cms.string("Flag_globalTightHalo2016Filter"),
                                applyfilter = cms.untracked.bool(True),
                                debugOn = cms.untracked.bool(False),
