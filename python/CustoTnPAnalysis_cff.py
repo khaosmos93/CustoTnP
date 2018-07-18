3c95a5033e796e288be24b7c2e948c5559bbc799
khaosmos93 <khaosmos93@gmail.com>
2018-07-18 08:36:57 +0200

    update python

--- python/CustoTnPAnalysis_cff.py
+++ python/CustoTnPAnalysis_cff.py
@@ -19,7 +19,7 @@ goodDataFiltersMiniAOD = [primaryVertexMiniAOD]
 
 from MuonPhotonMatch_cff import muonPhotonMatch, muonPhotonMatchMiniAOD
 
-leptonsMini = cms.EDProducer('CustoTnPLeptonProducer_miniAOD',
+leptons = cms.EDProducer('CustoTnPLeptonProducer_miniAOD',
                               muon_src = cms.InputTag('slimmedMuons'),
                               muon_srcSecond = cms.InputTag('slimmedMuons'),
                               muon_cuts = cms.string(''),
@@ -32,4 +32,18 @@ leptonsMini = cms.EDProducer('CustoTnPLeptonProducer_miniAOD',
                               bits = cms.InputTag("TriggerResults","","HLT"),#data
                               prescales = cms.InputTag("patTrigger"),
                               )
+
+leptonsAOD = cms.EDProducer('CustoTnPLeptonProducer_AOD',
+                                muon_src = cms.InputTag('slimmedMuonsCustom'),
+                                muon_srcSecond = cms.InputTag('slimmedMuonsCustom'),
+                                trigger_summary = cms.InputTag('selectedPatTriggerCustom'),
+                                muon_photon_match_src = cms.InputTag('muonPhotonMatchAOD'),
+                                muon_cuts = cms.string(muon_cuts),
+                                muon_track_for_momentum = cms.string('TunePNew'),
+                                muon_track_for_momentum_CSC = cms.string('Inner'),
+                                electron_muon_veto_dR = cms.double(-1),
+                                trigger_match_max_dR = cms.double(0.2),
+                                bits = cms.InputTag("TriggerResults","","HLT"),
+                                prescales = cms.InputTag("patTrigger"),
+                                )
 
