635e7ca75d277affefefcb4dc5262d8b61e18d1a
khaosmos93 <khaosmos93@gmail.com>
2018-07-21 15:48:02 +0200

    update python

--- python/CustoTnPAnalysis_cff.py
+++ python/CustoTnPAnalysis_cff.py
@@ -22,21 +22,20 @@ goodDataFiltersMiniAOD = [primaryVertexMiniAOD]
 
 from MuonPhotonMatch_cff import muonPhotonMatch, muonPhotonMatchMiniAOD
 
-leptons = cms.EDProducer('CustoTnPLeptonProducer_miniAOD',
+leptons = cms.EDProducer('CustoTnPLeptonProducer',
                               muon_src = cms.InputTag('slimmedMuons'),
                               muon_srcSecond = cms.InputTag('slimmedMuons'),
                               muon_cuts = cms.string(''),
                               muon_track_for_momentum = cms.string('TunePNew'),
                               muon_track_for_momentum_CSC = cms.string('Inner'),
                               muon_photon_match_src = cms.InputTag('muonPhotonMatchMiniAOD'),
-                              electron_muon_veto_dR = cms.double(-1),
                               trigger_match_max_dR = cms.double(0.2),
                               trigger_summary = cms.InputTag('slimmedPatTrigger'), # to run on 2017 Data
                               bits = cms.InputTag("TriggerResults","","HLT"),#data
                               prescales = cms.InputTag("patTrigger"),
                               )
 
-leptonsAOD = cms.EDProducer('CustoTnPLeptonProducer_AOD',
+leptonsAOD = cms.EDProducer('CustoTnPLeptonProducer',
                                 muon_src = cms.InputTag('slimmedMuonsCustom'),
                                 muon_srcSecond = cms.InputTag('slimmedMuonsCustom'),
                                 trigger_summary = cms.InputTag('selectedPatTriggerCustom'),
@@ -44,7 +43,6 @@ leptonsAOD = cms.EDProducer('CustoTnPLeptonProducer_AOD',
                                 muon_cuts = cms.string(''),
                                 muon_track_for_momentum = cms.string('TunePNew'),
                                 muon_track_for_momentum_CSC = cms.string('Inner'),
-                                electron_muon_veto_dR = cms.double(-1),
                                 trigger_match_max_dR = cms.double(0.2),
                                 bits = cms.InputTag("TriggerResults","","HLT"),
                                 prescales = cms.InputTag("patTrigger"),
